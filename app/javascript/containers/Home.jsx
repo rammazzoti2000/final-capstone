456d870da39b1629f8ed57315a00c0934b01699b
Alexandru <rammazzoti2000@gmail.com>
2020-09-11 14:50:40 +0200

    react: Implement SignUp  component to simulate the process

--- app/javascript/containers/Home.jsx
+++ app/javascript/containers/Home.jsx
@@ -8,38 +8,103 @@ export default () => (
       <nav className="navbar navbar-inverse bg-blue fixed-top">
         <h1 className="navbar-brand abs" href="#">Home</h1>
       </nav>
-      <div className="container text-content">
-        <h3>Fixed Navbar</h3>
-        <div className="row">
-          <div className="col-md-4">
-            <p>
-              A fixed navigation bar stays visible in a fixed position
-              (top or bottom) independent of the page scroll.
-            </p>
-            <p>
-              A fixed navigation bar stays visible in a fixed position
-              (top or bottom) independent of the page scroll.
-            </p>
-          </div>
-          <div className="col-md-4">
-            <p>
-              A fixed navigation bar stays visible in a fixed position
-              (top or bottom) independent of the page scroll.
-            </p>
-            <p>
-              A fixed navigation bar stays visible in a fixed position
-              (top or bottom) independent of the page scroll.
-            </p>
-          </div>
-          <div className="col-md-4">
-            <p>
-              A fixed navigation bar stays visible in a fixed position
-              (top or bottom) independent of the page scroll.
-            </p>
-            <p>
-              A fixed navigation bar stays visible in a fixed position
-              (top or bottom) independent of the page scroll.
-            </p>
+
+      <div className="container text-content sign-in-up">
+        <div className="row justify-content-center">
+          <div className="col-md-4 col-md-offset-4">
+            <br />
+            {/* Nav tabs */}
+            <div className="text-center">
+              <div className="btn-group">
+                <a href="#new" role="tab" data-toggle="tab" className="big btn btn-primary">
+                  <i className="fa fa-plus" />
+                  Create Account
+                </a>
+                <a href="#user" role="tab" data-toggle="tab" className="big btn btn-success">
+                  <i className="fa fa-user" />
+                  Login
+                </a>
+              </div>
+            </div>
+            <p className="click2select">Tap to select</p>
+            <div className="tab-content">
+              <div className="tab-pane fade in active" id="new">
+                <br />
+                <fieldset>
+                  <div className="form-group">
+                    <div className="right-inner-addon">
+                      <i className="fa fa-envelope" />
+                      <input className="form-control input-lg" placeholder="Email" type="text" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="right-inner-addon">
+                      <i className="fa fa-key" />
+                      <input className="form-control input-lg" placeholder="Password" type="password" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="right-inner-addon">
+                      <i className="fa fa-key" />
+                      <input className="form-control input-lg" placeholder="Confirm Password" id="" type="password" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <select className="custom-select custom-select-md">
+                      <option selected>Choose monthly units</option>
+                      <option value="1800">1800</option>
+                      <option value="2100">2100</option>
+                      <option value="2400">2400</option>
+                      <option value="2700">2700</option>
+                      <option value="3000">3000</option>
+                    </select>
+                  </div>
+                  <div className="form-group">
+                    <select className="custom-select custom-select-md">
+                      <option selected>Choose month target savings</option>
+                      <option value="5">5%</option>
+                      <option value="10">10%</option>
+                      <option value="15">15%</option>
+                      <option value="20">20%</option>
+                      <option value="25">25%</option>
+                    </select>
+                  </div>
+                </fieldset>
+                <hr />
+                <div className="tab-content">
+                  <div className="tab-pane active text-center" id="pp">
+                    <button type="button" className="btn btn-primary btn-lg btn-block">
+                      <i className="fa fa-plus" />
+                      Create Account
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="tab-pane fade" id="user">
+                <br />
+                <fieldset>
+                  <div className="form-group">
+                    <div className="right-inner-addon">
+                      <i className="fa fa-envelope" />
+                      <input className="form-control input-lg" placeholder="Email" type="text" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="right-inner-addon">
+                      <i className="fa fa-key" />
+                      <input className="form-control input-lg" placeholder="Password" type="password" />
+                    </div>
+                  </div>
+                </fieldset>
+                <br />
+                <div className=" text-center">
+                  <button type="button" className="btn btn-primary btn-success">
+                    <i className="fa fa-user" />
+                    LOGIN
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -67,5 +132,4 @@ export default () => (
       </footer>
     </div>
   </div>
-
 );
