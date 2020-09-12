80d190a42358cdcca0281002ff79593c215fe2ef
Alexandru <rammazzoti2000@gmail.com>
2020-09-12 16:03:59 +0200

    rails: Session - Implement Configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def login(user)
+    session[:id] = user.id
+  end
+
+  def current_user
+    @current_user ||= session[:id] && User.find_by_id(session[:id])
+  end
+
+  def logged_in?
+    render json: { code: 401 } unless current_user
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,2 +1,31 @@
 class Users::SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: %i[create destroy]
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      login @user
+      render json: {
+        code: 200,
+        user: {
+          name: @user.name
+        }
+      }
+    else
+      render json: {
+        code: 400,
+        errors: { Error: ['Email and/or Password does not match !'] }
+      }
+    end
+  end
+
+  def destroy
+    @current_user = nil
+    session.delete(:id)
+  end
+
+  private
+  def user_params
+    params[:user].permit(:email, :password)
+  end
 end
