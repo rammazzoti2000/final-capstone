c54726e88a36ab72f59d31cac4e9153523e637b6
Alexandru <rammazzoti2000@gmail.com>
2020-09-13 18:07:40 +0200

    rails: User/Reading - Add validations, add relationships

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,2 +1,12 @@
 class Reading < ApplicationRecord
+  belongs_to :user
+  validates :bedroom, presence: true
+  validates :study, presence: true
+  validates :garage, presence: true
+  validates :living, presence: true
+  validates :kitchen, presence: true
+  validates :guest, presence: true
+  validates :consumption, presence: true
+  validates :available, presence: true
+  validates :saved, presence: true
 end
