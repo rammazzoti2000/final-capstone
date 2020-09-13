b3076b9f867acf3ad28b116085c520748fa0804d
Alexandru <rammazzoti2000@gmail.com>
2020-09-13 18:11:20 +0200

    rails: Reading - Add reference to User Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_155728) do
+ActiveRecord::Schema.define(version: 2020_09_13_160936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_09_13_155728) do
     t.integer "saved"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_readings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_09_13_155728) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "readings", "users"
 end
