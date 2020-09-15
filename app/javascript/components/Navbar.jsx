fc44e26b634161cb7314d0da39959efac229f910
Alexandru <rammazzoti2000@gmail.com>
2020-09-15 13:18:01 +0200

    react: DayStats - Add trailcolor prop to styling

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -1,5 +1,12 @@
 /* eslint-disable no-return-assign, jsx-a11y/anchor-is-valid, import/no-named-as-default */
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChartBar,
+  faChartLine,
+  faChartPie,
+  faEllipsisH
+} from '@fortawesome/free-solid-svg-icons';
 import SignoutBtn from './home/Signout';
 
 const Navbar = () => (
@@ -8,27 +15,27 @@ const Navbar = () => (
       <ul className="row foot-row nav nav-pills tablist" role="tablist">
         <li className="col foot-col nav-item">
           <a onClick={() => window.location.href = '/addreading'} className="nav-link" data-toggle="pill" href="#">
-            <i className="fas fa-chart-bar" />
+            <FontAwesomeIcon icon={faChartBar} className="navIcon" />
             <p>Add Readings</p>
           </a>
         </li>
         <li className="col foot-col nav-item">
           <a onClick={() => window.location.href = '/readings'} className="nav-link" data-toggle="pill" href="#">
-            <i className="fas fa-chart-line" />
+            <FontAwesomeIcon icon={faChartLine} className="navIcon" />
             <p>Track.it</p>
           </a>
         </li>
         <li className="col foot-col nav-item">
           <a onClick={() => window.location.href = '/report'} className="nav-link" data-toggle="pill" href="#">
-            <i className="fas fa-chart-pie" />
+            <FontAwesomeIcon icon={faChartPie} className="navIcon" />
             <p>Report</p>
           </a>
         </li>
         <li className="col foot-col nav-item">
           <a className="nav-link" data-toggle="pill" href="#">
             <SignoutBtn />
-            {/* <i className="fas fa-ellipsis-h" />
-            <p>More</p> */}
+            <FontAwesomeIcon icon={faEllipsisH} className="navIcon" />
+            {/* <p>More</p> */}
           </a>
         </li>
       </ul>
